bba8cde2f7438652b0b227579130179b75ef2624
rafaelsousadevbr <rafael@sousalima.dev.br>
2025-10-29 00:00:49 -0300

    Criado o menu do banco e foi implementado todas as ações

--- src/BancoAlura.java
+++ src/BancoAlura.java
@@ -4,9 +4,11 @@ public class BancoAlura {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         String nomeDoCliente = "Veridiana dos Reis Lima";
         String tipoDeConta = "Corrente";
         double saldoDaConta = 2349.00;
+        int opcaoMenu = 0;
 
         System.out.println("****************************************************************");
         System.out.println("                       BANCO ALURA");
@@ -15,5 +17,40 @@ public class BancoAlura {
         System.out.println("Saldo Atual da Conta: R$ " + saldoDaConta);
         System.out.println("****************************************************************");
 
+        String menuDaConta = """
+                ****** Digite Operação Desejada ******
+                1 - Consultar Saldo da Conta
+                2 - Sacar valor
+                3 - Depositar valor
+                4 - Sair da Conta
+                **************************************
+                """;
+
+        while(opcaoMenu != 4){
+            System.out.println(menuDaConta);
+            opcaoMenu = scanner.nextInt();
+
+            if(opcaoMenu == 1) {
+                System.out.println("Saldo Atual da Conta: R$" + saldoDaConta);
+            } else if (opcaoMenu == 2){
+                System.out.println("Qual o valor desejado que pretende sacar:");
+                double valorDoSaque = scanner.nextDouble();
+                if(valorDoSaque > saldoDaConta){
+                    System.out.println("Não há saldo suficiente para saque!");
+                } else {
+                    saldoDaConta -=  valorDoSaque;
+                    System.out.println("Novo saldo da conta: R$" + saldoDaConta);
+                }
+            } else if (opcaoMenu == 3) {
+                System.out.println("Qual o valor que deseja depositar :");
+                double valorDoDeposito = scanner.nextDouble();
+                saldoDaConta += valorDoDeposito;
+                System.out.println("Novo saldo da conta: R$" + saldoDaConta);
+            } else if (opcaoMenu != 4) {
+                System.err.println("Opção inválida, digite novamente: ");
+            }
+        }
+
+        System.out.println("Saída da Conta Realizada com Sucesso!!!");
     }
 }
