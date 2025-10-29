6ac799c1ee826a9748db34dcbda312b341040896
rafaelsousadevbr <rafael@sousalima.dev.br>
2025-10-28 22:52:51 -0300

    Inicializado os Dados da Cliente

--- src/BancoAlura.java
+++ src/BancoAlura.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class BancoAlura {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         String nomeDoCliente = "Veridiana dos Reis Lima";
         String tipoDeConta = "Corrente";
         double saldoDaConta = 2349.00;
-
 
         System.out.println("****************************************************************");
         System.out.println("                       BANCO ALURA");
